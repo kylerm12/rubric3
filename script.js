e84e10341db09342e048fccb65cab7dbcc53c730
kyler <kylermartinez11@gmail.com>
2020-09-18 12:40:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var x;	
+x= prompt("Hey what's your name?");
+	alert("Hey "+x+" Nice to meet you!");
+
+
 var mydemo = document.getElementById("demoparent");
 console.log(mydemo.childNodes.length);
 
@@ -12,4 +17,14 @@ console.log ("number of li items in menu: " + linksinmenu.length);
 var mycomments = document.getElementById("comment");
 
 mycomments.setAttribute("align", "right");
+
+mycomments.setAttribute("style", "color: red");
+
+var newItem= document.createElement("li");
+
+var newtext= document.createTextNode("hey, this wasnt here before!");
+
+mydemo.appendChild(newItem);
+
+newItem.appendChild(newtext);
 // JavaScript Document
